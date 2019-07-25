555e8e1fdf03c47683c5c14d6f09955f915487b5
petervarga0819 <varga.peter0819@gmail.com>
2019-07-25 10:50:50 +0200

    legs done

--- AnimalsAndLegs.java
+++ AnimalsAndLegs.java
@@ -1,10 +1,22 @@
 package Practice;
-import
+import java.util.Scanner;
 public class AnimalsAndLegs {
     public static void main(String[] args) {
         // Write a program that asks for two integers
         // The first represents the number of chickens the farmer has
         // The second represents the number of pigs owned by the farmer
         // It should print how many legs all the animals have
+
+        Scanner chickens = new Scanner(System.in);
+        System.out.println("Type the number of chickens");
+        int numberOfChicken = chickens.nextInt();
+
+
+        Scanner pigs = new Scanner(System.in);
+        System.out.println("Type the number of pigs");
+        int numberOfPigs = pigs.nextInt();
+
+        System.out.println("They have " + ((numberOfChicken * 2) + (numberOfPigs * 4)) + " legs." );
+
     }
 }
